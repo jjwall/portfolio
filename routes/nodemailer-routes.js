87e062523babe14414b597b84ecee4b487f927b0
Jacob Wallace <jacob.wallace@utexas.edu>
2017-07-20 20:55:38 -0500

    Removed the now ignored nodemailer-routes file

--- routes/nodemailer-routes.js
+++
@@ -1,9 +0,0 @@
-var path = require("path");
-
-// Routes
-// =============================================================
-module.exports = function(app) {
-  app.post("/contact", function(req,res){
-    console.log("nodemailer post here");
-  });
-}
